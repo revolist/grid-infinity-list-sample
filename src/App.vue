cc2f7708c28d5c810e5761b5fe67c7f56c73802c
Revolist <64810943+revolist@users.noreply.github.com>
2021-03-18 09:38:27 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-grid
+    class="list"
+    theme="compact"
+    resize="false"
+    readonly="true"
+    :source="rows"
+    :columns="columns"
+  ></v-grid>
+</template>
+
+<script>
+import VGrid from "@revolist/vue-datagrid";
+
+function generateFakeDataRows(rowsNumber) {
+  const result = [];
+  const all = rowsNumber;
+  for (let j = 0; j < all; j++) {
+    let row = j;
+    if (!result[row]) {
+      result[row] = {};
+    }
+    result[row]["myRow"] = `I am row ${row}`;
+  }
+  return result;
+}
+
+export default {
+  name: "App",
+  data() {
+    return {
+      rows: generateFakeDataRows(10000),
+      columns: [
+        {
+          prop: "myRow",
+        },
+      ],
+    };
+  },
+  components: {
+    VGrid,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+.list .row {
+  display: flex;
+}
+
+.list .row .data-cell,
+.data-header-cell {
+  position: relative !important;
+  width: auto !important;
+}
+
+revogr-focus,
+.selection-border-range,
+.edit-input-wrapper {
+  width: 100% !important;
+}
+.header-wrapper {
+  display: none;
+}
+</style>
